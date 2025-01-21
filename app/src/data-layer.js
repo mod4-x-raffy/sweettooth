f6b05122a62c96013a017607b6d0fee8dce7ce0c
ffyruh <78766072+ffyruh@users.noreply.github.com>
2025-01-21 18:46:30 -0500

    STRECH: Implement other categories #14

--- app/src/data-layer.js
+++ app/src/data-layer.js
@@ -1,34 +1,89 @@
-let category = "Dessert";
-const manyURL = `https://themealdb.com/api/json/v1/1/filter.php?c=${category}`;
-const singleURL = `https://themealdb.com/api/json/v1/1/lookup.php?i=`
+// local storage implementations
+const setLocalStorage = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+const getLocalStorage = (key) => {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+const categoriesURL = `https://themealdb.com/api/json/v1/1/list.php?c=list`
+const categoryItemsURL = `https://themealdb.com/api/json/v1/1/filter.php?c=`;
+const detailsURL = `https://themealdb.com/api/json/v1/1/lookup.php?i=`
 const searchURL = `https://themealdb.com/api/json/v1/1/search.php?s=`
+const randomURL = `https://themealdb.com/api/json/v1/1/random.php`
 
-let randomFood = {};
+// NOTE: API Endpoints
+// Search by name
+//   www.themealdb.com/api/json/v1/1/search.php?s=${Name}
+// Lookup by id (details)
+//   www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}
+// Look up a random meal (details)
+// Not really needed, can just primitively randomize
+// for landing page "random" meal of the day
+//   www.themealdb.com/api/json/v1/1/random.php
+// List all categories
+//   www.themealdb.com/api/json/v1/1/list.php?c=list
+// Filter by category
+//   www.themealdb.com/api/json/v1/1/filter.php?c=${category}
+// List all areas
+//   www.themealdb.com/api/json/v1/1/list.php?a=list
+// Filter by area
+//   www.themealdb.com/api/json/v1/1/filter.php?a=${area}
+// Generate a single random meal
+//   https://www.themealdb.com/api/json/v1/1/random.php
 
-const fetchAllRecipes = async () => {
+// const fetchAllRecipes = async (category) => {
+//   try {
+//     const response = await fetch(categoryItemsURL+category);
+//     if (!response.ok) {
+//       throw new Error(`Error code ${response.status}`);
+//     }
+//     const categoryRecipes = await response.json();
+//     return categoryRecipes;
+//   } catch (error) {
+//     console.warn(error);
+//   }
+// };
+
+const fetchAllCategories = async () => {
   try {
-    const response = await fetch(manyURL);
+    const response = await fetch(categoriesURL);
     if (!response.ok) {
       throw new Error(`Error code ${response.status}`);
     }
-    const allRecipes = await response.json();
-    return allRecipes;
+    const categoriesList = await response.json();
+    console.log(categoriesList);
+    return categoriesList;
   } catch (error) {
-    console.error(error);
+    console.warn(error);
   }
-};
+}
+
+const fetchCategoryItems = async (category) => {
+  try {
+    const response = await fetch(categoryItemsURL+category);
+    if (!response.ok) {
+      throw new Error(`Error code ${response.status}`);
+    }
+    const categoriesItems = await response.json();
+    return categoriesItems;
+  } catch (error) {
+    console.warn(error);
+  }
+}
 
 const fetchSingleRecipe = async (id) => {
   try {
-    const response = await fetch(singleURL+id);
+    const response = await fetch(detailsURL+id);
     if (!response.ok) {
       throw new Error(`Error code ${response.status}`);
     }
-    const singleRecipe = await response.json();
-    console.log(singleRecipe);
-    return singleRecipe;
+    const recipeData = await response.json();
+    console.log(recipeData);
+    return recipeData;
   } catch (error) {
-    console.error(error);
+    console.warn(error);
   }
 }
 
@@ -38,17 +93,37 @@ const searchRecipe = async (name) => {
     if (!response.ok) {
       throw new Error(`Error code ${response.status}`);
     }
-    const searchedRecipe = await response.json();
-    console.log(searchedRecipe);
-    return searchedRecipe;
+    const recipeData = await response.json();
+    console.log(recipeData);
+    return recipeData;
   } catch (error) {
-    console.error(error);
+    console.warn(error);
+    return null;
+  }
+}
+
+const fetchRandomRecipe = async () => {
+  try {
+    const response = await fetch(randomURL);
+    if (!response.ok) {
+      throw new Error(`Error code ${response.status}`);
+    }
+    const recipeData = await response.json();
+    console.log(recipeData);
+    return recipeData;
+  } catch (error) {
+    console.warn(error);
     return null;
   }
 }
 
 export {
-  fetchAllRecipes,
+  // fetchAllRecipes,
+  setLocalStorage,
+  getLocalStorage,
   fetchSingleRecipe,
-  searchRecipe
-};
+  fetchCategoryItems,
+  fetchAllCategories,
+  fetchRandomRecipe,
+  searchRecipe,
+}
--- app/src/dom-helpers.js
+++ app/src/dom-helpers.js
@@ -1,3 +1,174 @@
+import {
+  fetchSingleRecipe,
+  fetchCategoryItems,
+  fetchAllCategories,
+  fetchRandomRecipe
+} from './data-layer.js'
+
+// -------------- EVENT HANDLERS -------------- //
+const handleRecipeClick = async (event) => {
+  const li = event.target.closest('li');
+  if (li === null) return;
+  console.log(li.dataset.idMeal);
+
+  const recipeData = await fetchSingleRecipe(li.dataset.idMeal);
+  renderSingleRecipe(recipeData);
+}
+
+const handleBannerClick = async (event) => {
+  const div = event.target.closest('div');
+  if (div === null) return;
+  console.log(div.dataset.idMeal);
+
+  const recipeData = await fetchSingleRecipe(div.dataset.idMeal);
+  renderSingleRecipe(recipeData);
+}
+
+const handleSearch = async (event) => {
+  event.preventDefault();
+  const form = event.target;
+  console.log(form.elements.dish.value);
+  const recipeData = await searchRecipe(form.elements.dish.value);
+  form.clear(); // TODO: confirm if this works
+  if (recipeData === null) {
+    renderErrorToast();
+    return;
+  }
+  renderSingleRecipe(recipeData);
+}
+
+const collapseAllCategories = () => {
+  const contentDivs = document.querySelectorAll('div.category-content');
+  for (const div of contentDivs) {
+    if (div.classList.contains('collapsed')) continue;
+    div.classList.add('collapsed');
+  }
+}
+
+const handleCategoryExpand = async (event) => {
+  const div = event.target.closest('div');
+  if (!div.classList.contains('category-header')) return;
+
+  // TODO: Expand this category
+  const contentDiv = div.nextElementSibling;
+  if (contentDiv.classList.contains('collapsed')) collapseAllCategories();
+  const categoryLI = event.target.closest('li');
+  console.log(contentDiv);
+  if (!contentDiv.classList.contains('collapsed')) {
+    contentDiv.classList.add('collapsed');
+    return;
+  }
+
+  if (contentDiv.innerHTML.trim() === '') {
+    console.log(contentDiv.innerHTML);
+    const categoryItems = await fetchCategoryItems(categoryLI.dataset.category);
+
+    const categoryItemsUL = document.createElement('ul');
+    categoryItemsUL.classList.add('category-items');
+
+    console.log(categoryItems);
+    for (const [index, recipe] of categoryItems.meals.entries()) {
+      const li = document.createElement('li');
+      li.id = index;
+      li.classList.add('category-item')
+      // TODO: not sure about this one
+      // li.dataset.category = categoryLI.dataset.category;
+
+      const img = document.createElement('img');
+      img.src = recipe.strMealThumb;
+      img.alt = recipe.strMeal;
+
+      const p = document.createElement("p");
+      p.textContent = recipe.strMeal;
+
+      li.append(img, p);
+      categoryItemsUL.append(li);
+    }
+
+    contentDiv.append(categoryItemsUL);
+  }
+  console.log(contentDiv.classList);
+  contentDiv.classList.remove('collapsed');
+  console.log(contentDiv.classList);
+}
+
+// -------------- RENDER FUNCS -------------- //
+const initLanding = () => {
+  const main = document.querySelector('main');
+  main.innerHTML = `
+<section id="recipe-banner">
+  <!-- item here will be handled in js -->
+</section>
+<section id="categories-section">
+  <ul id="categories-list">
+    <!-- items here will be handled in js -->
+  </ul>
+</section>
+`;
+}
+
+const renderLanding = async () => {
+  // TODO: Refactor into showing categories instead
+
+  // if coming from details page.
+  initLanding();
+  await renderRecipeOfTheDay();
+  await renderAllCategories();
+
+  // attach listeners
+  const recipeOfTheDay = document.querySelector('div#recipe-container');
+  recipeOfTheDay.addEventListener('click', handleBannerClick);
+
+  const searchBox = document.querySelector('form#search-box');
+  searchBox.addEventListener('submit', handleSearch);
+
+  const mainMenuButton = document.querySelector('button#main-menu');
+  mainMenuButton.addEventListener('click', rerenderLanding);
+
+  const categoriesListUL = document.querySelector('ul#categories-list');
+  categoriesListUL.addEventListener('click', handleCategoryExpand)
+}
+
+const renderCategoryItems = async () => {
+  // TODO:
+  // 1. fetch all items from this category
+  // 2. append the data
+  // 3. maybe render?
+}
+
+const renderAllCategories = async () => {
+  const categoriesList = await fetchAllCategories();
+  const categoriesListUL = document.querySelector('ul#categories-list')
+  for (const [index, category] of categoriesList.meals.entries()) {
+    const li = document.createElement('li');
+    li.id = index;
+    li.dataset.category = category.strCategory;
+
+    // -------------- CATEGORY HEADER DIV -------------- //
+    const headerDiv = document.createElement('div');
+    headerDiv.classList.add('category-header');
+    const headerText = document.createElement('h3');
+    headerText.textContent = category.strCategory;
+    // finalize header div 
+    headerDiv.append(headerText);
+    li.append(headerDiv);
+
+
+    // -------------- CATEGORY CONTENT DIV -------------- //
+    const contentDiv = document.createElement('div');
+    contentDiv.classList.add('category-content');
+    contentDiv.classList.add('collapsed');
+    // finalize content div, collapsed, initially
+    li.append(contentDiv)
+    // finalize li into parent UL
+    categoriesListUL.append(li);
+  }
+}
+
+const rerenderLanding = async () => {
+  if (document.querySelector('section#categories-section') === null) renderLanding();
+}
+
 const renderAllRecipes = (allRecipes) => {
   const recipesUL = document.querySelector("ul#dish-catalog");
   recipesUL.innerHTML = '';
@@ -18,20 +189,22 @@ const renderAllRecipes = (allRecipes) => {
   });
 };
 
-const renderRecipeOfDay = (recipe) => {
+const renderRecipeOfTheDay = async () => {
+  let recipeData = await fetchRandomRecipe();
+  recipeData = recipeData.meals[0];
   const section = document.querySelector("section#recipe-banner");
   section.innerHTML = '';
 
   const div = document.createElement('div');
-  div.id = 'recipe-banner';
-  div.dataset.idMeal = recipe.idMeal;
+  div.id = 'recipe-container';
+  div.dataset.idMeal = recipeData.idMeal;
 
   const h1 = document.createElement("h1");
-  h1.textContent = `Recipe of the day: ${recipe.strMeal}`;
+  h1.textContent = `Recipe of the day: ${recipeData.strMeal}`;
 
   const img = document.createElement("img");
-  img.src = recipe.strMealThumb;
-  img.alt = recipe.strMeal;
+  img.src = recipeData.strMealThumb;
+  img.alt = recipeData.strMeal;
 
   div.append(img, h1);
   section.append(div);
@@ -152,14 +325,15 @@ const renderSingleRecipe = (recipeData) => {
   main.append(recipe);
 };
 
-// TODO: render error toast
+// TODO: Render Error Toast
+// make it so that all errors in fetching pass through here
 const renderErrorToast = () => {
 
 }
 
 export {
+  renderLanding,
   renderAllRecipes,
-  renderRecipeOfDay,
   renderSingleRecipe,
   renderErrorToast
 };
