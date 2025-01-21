15b6faa8d88f9c32ef4a60656a87640b51cc379c
XavMontana <xavierhertzog@gmail.com>
2025-01-21 17:25:31 -0500

    I added a button as the title

--- app/main.js
+++ app/main.js
@@ -1,23 +1,31 @@
-import { renderAllRecipes, renderRecipeOfDay, renderSingleRecipe } from "/src/dom-helpers.js";
-import { fetchAllRecipes, fetchSingleRecipe, searchRecipe } from "/src/data-layer.js";
+import {
+  renderAllRecipes,
+  renderRecipeOfDay,
+  renderSingleRecipe,
+} from "/src/dom-helpers.js";
+import {
+  fetchAllRecipes,
+  fetchSingleRecipe,
+  searchRecipe,
+} from "/src/data-layer.js";
 
 const handleRecipeClick = async (event) => {
-  const li = event.target.closest('li');
+  const li = event.target.closest("li");
   if (li === null) return;
   console.log(li.dataset.idMeal);
 
   const recipeData = await fetchSingleRecipe(li.dataset.idMeal);
   renderSingleRecipe(recipeData);
-}
+};
 
 const handleBannerClick = async (event) => {
-  const div = event.target.closest('div');
+  const div = event.target.closest("div");
   if (div === null) return;
   console.log(div.dataset.idMeal);
 
   const recipeData = await fetchSingleRecipe(div.dataset.idMeal);
   renderSingleRecipe(recipeData);
-}
+};
 
 const handleSearch = async (event) => {
   event.preventDefault();
@@ -29,10 +37,10 @@ const handleSearch = async (event) => {
     return;
   }
   renderSingleRecipe(recipeData);
-}
+};
 
 const initMain = () => {
-  const main = document.querySelector('main');
+  const main = document.querySelector("main");
   main.innerHTML = `
     <section id="recipe-banner">
       <!-- item here will be handled in js -->
@@ -46,10 +54,10 @@ const initMain = () => {
       </div>
     </section>
     `;
-}
+};
 
 const handleMainMenu = async () => {
-  if (document.querySelector('section#recipe-list') === null) initMain();
+  if (document.querySelector("section#recipe-list") === null) initMain();
   const allRecipes = await fetchAllRecipes();
   renderAllRecipes(allRecipes);
   const randomFood =
@@ -57,19 +65,22 @@ const handleMainMenu = async () => {
   renderRecipeOfDay(randomFood);
 
   // attach listeners
-  const recipeOfTheDay = document.querySelector('section#recipe-banner');
-  recipeOfTheDay.addEventListener('click', handleBannerClick);
+  const recipeOfTheDay = document.querySelector("section#recipe-banner");
+  recipeOfTheDay.addEventListener("click", handleBannerClick);
 
-  const recipesUL = document.querySelector('ul#dish-catalog');
-  console.log(recipesUL)
-  recipesUL.addEventListener('click', handleRecipeClick);
+  const recipesUL = document.querySelector("ul#dish-catalog");
+  console.log(recipesUL);
+  recipesUL.addEventListener("click", handleRecipeClick);
 
-  const searchBox = document.querySelector('form#search-box');
-  searchBox.addEventListener('submit', handleSearch);
+  const searchBox = document.querySelector("form#search-box");
+  searchBox.addEventListener("submit", handleSearch);
 
-  const mainMenuButton = document.querySelector('button#main-menu');
-  mainMenuButton.addEventListener('click', handleMainMenu)
-}
+  //const mainMenuButton = document.querySelector("button#main-menu");
+  //mainMenuButton.addEventListener("click", handleMainMenu);
+
+  const menuButton = document.querySelector("button#Main-Menu");
+  menuButton.addEventListener("click", handleMainMenu);
+};
 
 const main = async () => {
   handleMainMenu();
